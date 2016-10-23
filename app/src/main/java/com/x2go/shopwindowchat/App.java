d19a931e307adfff43ba85d85746057c05b8bdc9
Roberto Allende <rallende@gmail.com>
2016-10-23 17:47:44 -0300

    Adding most of the project's code.

---
+++ app/src/main/java/com/x2go/shopwindowchat/App.java
@@ -0,0 +1,8 @@
+package com.x2go.shopwindowchat;
+
+/**
+ * Created by Roberto Allende on 10/23/16.
+ */
+
+public class App {
+}
---
+++ app/src/main/java/com/x2go/shopwindowchat/AppPreferences.java
@@ -0,0 +1,12 @@
+package com.x2go.shopwindowchat;
+
+/**
+ * Created by Roberto Allende on 10/23/16.
+ */
+
+public class AppPreferences {
+
+    public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
